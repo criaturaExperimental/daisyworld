957f9a364665ce2b816e9d0b23de836b13f89b35
Lucas deGomez <lucasdegomez@gmail.com>
2017-08-16 16:28:12 +0200

    Reordering

--- app/sketch.js
+++ app/sketch.js
@@ -1,3 +1,5 @@
+// GLOBALS
+
 var radius;
 var color_center;
 var color_trunk;
@@ -7,39 +9,62 @@ var color_petal;
 var petal_wide;
 var petal_long;
 var petals;
-var petal_separation;
+var bck_color;
+var hill_color;
+
+function coloring() {
+    // COLORS
+    color_center = color(random(0,255), random(0,255), random(0,255));
+    color_petal = color(random(0,255), random(0,255), random(0,255));
+    color_trunk = color(random(0,255), random(0,255), random(0,255));
+    bck_color = color(random(0,20), random(0,20), random(50,255));
+    hill_color = color(random(0,250), random(0,20), random(0, 255));
+}
 
 function setup() {
     createCanvas(800, 640);
-    radius = random(20, 80);
-    console.log(radius);
-    color_center = color(random(0,255), random(0,255), random(0,255));
-    color_trunk = color(random(0,255), random(0,255), random(0,255));
-    color_petal = color(random(0,255), random(0,255), random(0,255));
+    translate(width/2, height/2);
 
-    trunk_height = random(20, height/2);
+    coloring();
+    createPlanet(bck_color, hill_color);
+    createDaisy(color_center, color_petal, color_trunk);
+
+}
+
+function draw() {
+
+}
+
+function createPlanet(bck_color, hill_color) {
+    noStroke();
+    background(bck_color);
+    fill(hill_color);
+    ellipse(0, height/2-50, width*1.5, height);
+}
+
+
+function createDaisy(color_center, color_petal, color_trunk) {
+    // DIMENSIONS
+    radius = random(20, 80);
+    trunk_height = random(radius*2, height/2);
     trunk_wide = random(10, radius/2)
     petals = ceil(random(7, 15));
-    petal_separation = 80;
     petal_long = random(trunk_height*0.4, trunk_height*0.8 - radius);
     petal_wide = random(trunk_wide*1.4, trunk_wide*2);
 
-    noStroke();
-    translate(width/2, height/2)
-
+    // BLOOM!
+    stroke(1);
     fill(color_trunk);
     rect(0-(trunk_wide/2), 0, trunk_wide, trunk_height);
 
+    noStroke();
     fill(color_petal);
     for (var i = 0; i < petals; i++) {
         rect(0, 0 + radius/4, petal_wide, petal_long);
         rotate((2*PI)/petals);
     }
 
+    stroke(2);
     fill(color_center);
     ellipse(0, 0, radius, radius);
-}
-
-function draw() {
-
 }
